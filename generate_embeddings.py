3802019570fade189b17f162361ea41a82fa75c0
David Shapiro <daveshap37@gmail.com>
2022-09-26 15:56:55 -0400

    trying a faster approach

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -12,29 +12,50 @@ def open_file(filepath):
         return infile.read()
 
 
-def save_data(payload):
+def save_data(direcotry, payload):
     filename = '%s.json' % str(uuid4())
-    with open('embeddings/%s' % filename, 'w', encoding='utf-8') as outfile:
+    with open('%s/%s' % (directory, filename), 'w', encoding='utf-8') as outfile:
         json.dump(payload, outfile, ensure_ascii=False, sort_keys=True, indent=1)
 
 
-if __name__ == '__main__':
-    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
-    arxiv = open_file('c:/arxiv/arxiv-metadata-oai-snapshot.json').splitlines()
-    print('Articles loaded:', len(arxiv))
-    errors = list()
-    count = 0
-    for article in arxiv:
-        count = count + 1
-        try:
+def process_chunk(chunk):
+    try:
+        articles = list()
+        strings = list()
+        for article in chunk:
             info = json.loads(article)
             title = re.sub('\s+', ' ', info['title'].strip())
-            print(count, title)
+            #print(title)
             abstract = re.sub('\s+', ' ', info['abstract'].strip())
-            embeddings = embed([title + ' ' + abstract])
-            vector = embeddings.numpy().tolist()[0]
-            save_data({'title': title, 'abstract': abstract, 'embedding': vector})
-        except:
-            errors.append(article)
-    with open('errors.json', 'w', encoding='utf-8') as outfile:
-        json.dump(errors, outfile, ensure_ascii=False, sort_keys=True, indent=1)
+            string = title + ' ' + abstract
+            articles.append({'id':article['id'], 'title':title, 'abstract':abstract)
+            strings.append(string)    
+        embeddings = embed(strings)  # try to do 100 embeddings at a time
+        vectors = embeddings.numpy().tolist()
+        for i in list(range(0, len(chunk))):
+            article = articles[i]
+            article['embedding'] = vectors[i]
+            save_data('embeddings', article)
+    except:
+        save_data('errors', chunk)
+
+
+if __name__ == '__main__':
+    embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder-large/5')
+    arxiv = open_file('c:/arxiv/arxiv-metadata-oai-snapshot.json').splitlines()
+    print('Articles loaded:', len(arxiv))
+    chunk_size = 100
+    chunks = [arxiv[i:i + chunk_size] for i in range(0, len(arxiv), chunk_size)]
+    total = len(chunks)
+    print('Chunks to process:', total)
+    arxiv = list()
+    count = 0
+    start = time()
+    for chunk in arxiv:
+        count = count + 1
+        process_chunk(chunk)
+        elapsed = time() - start
+        avg = elapsed / count
+        remaining = (total - count) * avg
+        hours = remaining / 3600
+        print(count, total - count, hours)
